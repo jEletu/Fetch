a9689fd1f87468492acb8077d0e1d059a7d13c7c
jEletu <jemimaheletu@gmail.com>
2026-02-09 14:39:30 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -10,3 +10,19 @@ fetch('https://dummyjson.com/users/1')
     });
 //response.json() converts the response to a JavaScript object.
 //Template literals (${}) are used to insert dynamic data into the HTML.
+
+    fetch('https://dummyjson.com/users')
+    .then(response => response.json())
+    .then(data => {
+        const staffList = document.getElementById('staffList');
+        data.users.forEach(user => {
+            const staffCard = document.createElement('div');
+            staffCard.innerHTML = `
+                <h2>${user.firstName} ${user.lastName}</h2>
+                <a href="details.html?id=${user.id}">View Details</a>
+            `;
+            staffList.appendChild(staffCard);
+        });
+    });
+//data.users contains the array of staff members.
+//forEach is used to loop through each user and dynamically create HTML elements.
